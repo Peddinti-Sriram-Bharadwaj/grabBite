1c8f6e2ca5de71a6dd66fb78b31f87def94a49cb
Sriram Bhdaradwaj <sriram9217@gmail.com>
2024-03-09 12:29:43 +0530

    homescreeen added

---
+++ screens/HomeScreen.js
@@ -0,0 +1,44 @@
+import { View, Text, Image } from 'react-native';
+import {useLayoutEffect} from 'react';
+import {useNavigation} from "@react-navigation/native";
+// this safe area view works for expo apps, not the orignial componet from react-native
+import { SafeAreaView } from 'react-native-safe-area-context';
+import { UserIcon, ChevronDownIcon, SearchIcon, AdjustmentsIcon} from 'react-native-heroicons/outline';
+
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+
+    useLayoutEffect(() => {{
+
+        navigation.setOptions({
+            headerShown: false
+        });
+    }}, [])
+
+
+    return (
+        <SafeAreaView className='bg-white pt-5'>
+            <View className='flex-row pb-3 items-center mx-4 space-x-2'>
+                <Image
+                    source={{ uri: 'https://links.papareact.com/wru' }}
+                    className='h-7 w-7 bg-gray-300 p-4 rounded-full'
+                />
+
+                <View className='flex-1'>
+                    <Text className="font-bold text-gray-400 text-xs">
+                        Deliver Now!
+                    </Text>
+                    <Text className="font-bold text-xl">
+                        CurrentLocation
+                        <ChevronDownIcon size={30} color="#00CCBB" />
+                    </Text>
+                </View>
+
+                <UserIcon size={35} color="#00CCBB" />
+            </View>
+        </SafeAreaView>
+    )
+    };
+
+export default HomeScreen;
