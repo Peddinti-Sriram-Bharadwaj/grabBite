58962cd4b2ad6e0dc0c4a2e7ca958d6ab92243c9
Sriram Bhdaradwaj <sriram9217@gmail.com>
2024-03-09 17:46:24 +0530

    Featured Rows added

---
+++ components/CategoryCard.js
@@ -0,0 +1,22 @@
+import { View, Text, TouchableOpacity, Image } from 'react-native'
+
+const CategoryCard = ({imgUrl, title }) => {
+  return (
+    <TouchableOpacity >
+        <Image 
+        source = {{
+            uri: imgUrl,
+        }}
+        className= 'h-20 w-20 rounded'
+        
+        /> 
+        <View>
+            <Text className = 'absolute bottom-1 left-1 text-white font-bold'>{title}</Text>
+        </View>
+
+    </TouchableOpacity>
+
+  )
+}
+
+export default CategoryCard
