4b6ba834a7b28a57d2780396ca51fdb8f9e65048
Sriram Bhdaradwaj <sriram9217@gmail.com>
2024-03-13 10:56:57 +0530

    basketslice is ready to use with addition and removal reducers

--- App.js
+++ App.js
@@ -5,15 +5,20 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import HomeScreen from './screens/HomeScreen';
 import RestaurantScreen from './screens/RestaurantScreen';
 const Stack = createNativeStackNavigator();
+import {Provider} from 'react-redux'
+import { store } from './store';
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name = "Home" component = {HomeScreen} />
-        <Stack.Screen name = "Restaurant" component = {RestaurantScreen} />
-      </Stack.Navigator>
-    </NavigationContainer>
+    <Provider store = {store}>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name = "Home" component = {HomeScreen} />
+          <Stack.Screen name = "Restaurant" component = {RestaurantScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    </Provider>
+
 
   );
 }
--- components/DishRow.js
+++ components/DishRow.js
@@ -3,9 +3,26 @@ import React, { useState } from 'react'
 import Currency from 'react-currency-formatter'
 import { urlFor } from '../sanity'
 import {MinusCircleIcon, PlusCircleIcon} from 'react-native-heroicons/solid'
+import {useDispatch, useSelector} from 'react-redux'
+import { addToBasket,
+   selectBasketItems,
+   removeFromBasket,
+    selectBasketItemsWithId } from '../features/basketSlice'
 
 const DishRow = ({id,name,description, price, image}) => {
     const [isPressed, setIsPressed] = useState(false);
+    const items = useSelector(state => selectBasketItemsWithId(state,id));
+    const dispatch = useDispatch();
+    const addItemToBasket = () => {
+
+      dispatch(addToBasket({id,name,description, price, image}))
+    }
+    const removeItemFromBasket = () => {
+      if (!items.length > 0) return;
+      dispatch(removeFromBasket({id}))
+
+    }
+    
 
   return (
     <>
@@ -13,7 +30,7 @@ const DishRow = ({id,name,description, price, image}) => {
       className = {`bg-white border p-4 border-gray-200 ${isPressed && "border-b-0"}`}>
         <View className = 'flex-row '>
         <View className = 'flex-1 pr-2 '>
-            <Text className = 'text-lg mb-1'>{name}</Text>
+            <Text className = 'text-lg lb-1'>{name}</Text>
             <Text className = 'text-gray-400'> {description}</Text>
             <Text className = 'text-gray-400 mt-2'>
                 <Currency quantity = {price} currency = 'INR' /> 
@@ -39,15 +56,18 @@ const DishRow = ({id,name,description, price, image}) => {
       {isPressed && (
         <View className = 'bg-white px-4'> 
             <View className = 'flex-row items-center space-x-2 pb-3'>
-                <TouchableOpacity>
+                <TouchableOpacity disabled = {!items.length} onPress = {removeItemFromBasket}>
                     <MinusCircleIcon
-                    color = '#00CCBB'
+                    color = {items.length >0 ? '#00CCBB' : "gray"}
                     size = {40} />
                 </TouchableOpacity>
-                <Text>0</Text>
+                <Text>{items.length}</Text>
 
-                <TouchableOpacity>
-                    <PlusCircleIcon  color = '#00CCBB' size = {40} />
+                <TouchableOpacity onPress = {addItemToBasket}  >
+                    <PlusCircleIcon
+                    
+                    color = '#00CCBB' 
+                    size = {40} />
                 </TouchableOpacity>
             </View>
         </View>
